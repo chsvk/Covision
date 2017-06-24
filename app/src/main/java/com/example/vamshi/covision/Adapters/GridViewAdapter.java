d7e6f8264d3739fb615e24c5947e8673089cd437
Vamshi Krishna <chsvk21@gmail.com>
2017-06-25 00:15:41 +0530

    Everything is working fine for no reason at all

--- app/src/main/java/com/example/vamshi/covision/Adapters/GridViewAdapter.java
+++ app/src/main/java/com/example/vamshi/covision/Adapters/GridViewAdapter.java
@@ -1,33 +1,41 @@
 package com.example.vamshi.covision.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.example.vamshi.covision.Data.ImageCache;
 import com.example.vamshi.covision.Data.TopTrack;
+import com.example.vamshi.covision.ImageDownloadTask;
 import com.example.vamshi.covision.R;
 
+import java.awt.font.TextAttribute;
 import java.util.List;
 
 /**
  * Created by Vamshi on 6/24/2017.
  */
-
+@GlideModule
 public class GridViewAdapter extends BaseAdapter {
 
     private List<TopTrack> tracks;
     private Context context;
     private LayoutInflater inflater;
+    private ImageCache imageCache;
 
 
     public GridViewAdapter(List<TopTrack> tracks, Context context) {
         this.tracks = tracks;
         this.context = context;
+        imageCache=ImageCache.Instance();
     }
 
     @Override
@@ -47,6 +55,7 @@ public class GridViewAdapter extends BaseAdapter {
     }
 
     @Override
+
     public View getView(int position, View convertView, ViewGroup parent) {
         View gridView = convertView;
 
@@ -56,9 +65,19 @@ public class GridViewAdapter extends BaseAdapter {
         }
 
         ImageView myImageView = (ImageView)gridView.findViewById(R.id.myImageView);
+        TextView temp = (TextView)gridView.findViewById(R.id.temptext);
         TopTrack tempObject = tracks.get(position);
         Log.i("IMAHE LNK", tempObject.getImageUrl());
+
         Glide.with(context).load(tempObject.getImageUrl().trim()).into(myImageView);
+        String song_name=tempObject.getSongName();
+//        if(imageCache.HasKey(song_name))
+//            myImageView.setImageBitmap(imageCache.GetFromCache(song_name));
+//        else
+//        {
+//            new ImageDownloadTask(myImageView,imageCache,song_name).execute(tempObject.getImageUrl());
+//        }
+        temp.setText(song_name);
 
         return gridView;
     }
